e4d94d56aa1af424a287af8ef9fd38e8cc43f0fa
dawson8 <csdawson8@gmail.com>
2022-04-24 22:19:35 +0000

    Episode 12

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -17,6 +17,26 @@ class Variation extends Model
         return Money::GBP($this->price);
     }
 
+    public function inStock()
+    {
+        return $this->stockCount() > 0;
+    }
+
+    public function outOfStock()
+    {
+        return !$this->inStock();
+    }
+
+    public function lowStock()
+    {
+        return !$this->outOfStock() && $this->stockCount() < 5;
+    }
+
+    public function stockCount()
+    {
+        return $this->descendantsAndSelf->sum(fn ($variation) => $variation->stocks->sum('amount'));
+    }
+
     public function stocks()
     {
         return $this->hasMany(Stock::class);
