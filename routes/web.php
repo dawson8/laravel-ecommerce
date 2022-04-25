ae010160da809dcdb43e7e6103568d56367b4eb1
Chris Dawson <csdawson8@gmail.com>
2022-04-25 22:37:30 +0100

    Episode 30

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartIndexController;
+use App\Http\Controllers\CategoryShowController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductShowController;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 Route::get('/cart', CartIndexController::class);
+Route::get('/categories/{category:slug}', CategoryShowController::class);
 Route::get('/products/{product:slug}', ProductShowController::class);
 
 Route::get('/dashboard', function () {
