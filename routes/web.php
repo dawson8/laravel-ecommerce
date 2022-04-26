158fdbc0ebb409304c55429c1ec6ea88efb4156c
Chris Dawson <csdawson8@gmail.com>
2022-04-26 19:40:38 +0100

    Episode 51

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomeController::class)->name('home');
-Route::get('/cart', CartIndexController::class);
+Route::get('/cart', CartIndexController::class)->name('cart');
 Route::get('/categories/{category:slug}', CategoryShowController::class);
 Route::get('/products/{product:slug}', ProductShowController::class);
 Route::get('/checkout', CheckoutIndexController::class);
