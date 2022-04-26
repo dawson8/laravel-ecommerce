5777c8f8c3648d779ab0e42439667b2bc55b1d11
dawson8 <csdawson8@gmail.com>
2022-04-26 11:45:55 +0100

    Episode 37

--- app/Http/Livewire/ProductBrowser.php
+++ app/Http/Livewire/ProductBrowser.php
@@ -8,6 +8,17 @@ use Livewire\Component;
 class ProductBrowser extends Component
 {
     public $category;
+    public $queryFilters = [];
+
+    public function mount()
+    {
+        $this->queryFilters = $this->category->products->pluck('variations')
+            ->flatten()
+            ->groupBy('type')
+            ->keys()
+            ->mapWithKeys(fn ($key) => [$key => []])
+            ->toArray();
+    }
 
     public function render()
     {
