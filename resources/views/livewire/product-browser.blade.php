426f581948f07e6f0bce0e319cfaf369d65941ae
dawson8 <csdawson8@gmail.com>
2022-04-26 14:12:57 +0100

    Episode 42

--- resources/views/livewire/product-browser.blade.php
+++ resources/views/livewire/product-browser.blade.php
@@ -14,12 +14,14 @@
             </div>
 
             <div class="space-y-6">
-                <div class="space-y-1">
-                    <div class="font-semibold">Max price ({{ money($priceRange['max']) }})</div>
-                    <div class="flex items-center space-x-2">
-                        <input type="range" min="0" max="{{ $maxPrice }}" wire:model="priceRange.max">
+                @if ($category->products->count())
+                    <div class="space-y-1">
+                        <div class="font-semibold">Max price ({{ money($priceRange['max']) }})</div>
+                        <div class="flex items-center space-x-2">
+                            <input type="range" min="0" max="{{ $maxPrice }}" wire:model="priceRange.max">
+                        </div>
                     </div>
-                </div>
+                @endif
 
                 @if ($products->count())
                     @foreach ($filters as $title => $filter)
