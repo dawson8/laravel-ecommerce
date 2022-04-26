0c2784f5ca3159313c54ccac13545ddebb0cdb72
Chris Dawson <csdawson8@gmail.com>
2022-04-26 19:07:23 +0100

    Episode 50

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -21,12 +21,14 @@
             <div class="space-y-3">
                 <div class="font-semibold text-lg">Shipping</div>
 
-                <x-select class="w-full" wire:model="userShippingAddressId">
-                    <option value="">Choose a pre-saved address</option>
-                    @foreach ($this->userShippingAddresses as $address)
-                        <option value="{{ $address->id }}">{{ $address->formattedAddress() }}</option>
-                    @endforeach
-                </x-select>
+                @if ($this->userShippingAddresses)
+                    <x-select class="w-full" wire:model="userShippingAddressId">
+                        <option value="">Choose a pre-saved address</option>
+                        @foreach ($this->userShippingAddresses as $address)
+                            <option value="{{ $address->id }}">{{ $address->formattedAddress() }}</option>
+                        @endforeach
+                    </x-select>
+                @endif
 
                 <div class="space-y-3">
                     <div>
