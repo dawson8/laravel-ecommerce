f7ac5b9b8de1de7dbdcebed4063b149c0bdc8ea7
Chris Dawson <csdawson8@gmail.com>
2022-04-27 16:37:27 +0100

    Episode 66

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -2,7 +2,9 @@
 
 namespace App\Observers;
 
+use App\Mail\OrderStatusUpdated;
 use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
 
 class OrderObserver
 {
@@ -18,8 +20,8 @@ class OrderObserver
             ->only($order->statuses)
             ->filter(fn ($status) => filled($status));
 
-        if ($originalOrder->status() === $order->status && $filledStatuses->count()) {
-            dd('send email');
+        if ($originalOrder->status() === $order->status() && $filledStatuses->count()) {
+            Mail::to($order->user)->send(new OrderStatusUpdated($order));
         }
     }
 }
